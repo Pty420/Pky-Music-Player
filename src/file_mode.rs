96ced8c688e4f0015281368461e45a2c814d7905
Pty420 <2606027122@qq.com>
2026-04-15 19:09:35 +0800

    重写了有关play命令的大部分代码，并且在状态面板添加了音频名称,扩大了各个面板的大小

--- src/file_mode.rs
+++ src/file_mode.rs
@@ -1,73 +1,108 @@
+use crate::panels::mode_selected;
 use crate::utilitys::check_char::check_head_char;
+use clearscreen::clear;
 use dirs::audio_dir;
 use rodio::Sink;
-use std::thread;
-use std::time;
-pub struct BackPathMode(pub String, pub bool);
+use std::{path::PathBuf, thread, time};
+pub struct BackPathMode(pub PathBuf, pub bool);
 pub fn play_cmd() -> BackPathMode {
     loop {
+        clear().unwrap();
+        mode_selected();
         let mut line = String::new();
-        std::io::stdin().read_line(&mut line).expect("无效指令！");
+        std::io::stdin().read_line(&mut line).expect("未知错误！");
         let line = line.trim();
         let mut s_line = line.split_whitespace();
-        if let Some(play) = s_line.next() {
-            if play == "play" {
-                let mut path = String::new();
-                let is_loop = match s_line.next() {
-                    Some("-l") => true,
+        match s_line.next() {
+            Some("play") => {
+                let mut full_path = PathBuf::new();
+                let mut line_cp = s_line.clone();
+                let is_loop = match line_cp.next() {
+                    Some("-l") => {
+                        let mut path = PathBuf::new();
+                        let mut path_str = String::new();
+                        loop {
+                            match line_cp.next() {
+                                Some(str) => {
+                                    if !path_str.is_empty() {
+                                        path_str.push_str(" ");
+                                    }
+                                    path_str = path_str + &str;
+                                    continue;
+                                }
+                                None => {
+                                    path.push(path_str);
+                                    break;
+                                }
+                            }
+                        }
+                        if check_head_char(&path.to_string_lossy(), '/') {
+                            full_path.push(path);
+                        } else {
+                            full_path =
+                                PathBuf::from(audio_dir().unwrap().to_string_lossy().to_string());
+                            full_path.push(path);
+                        }
+                        true
+                    }
                     Some(other) => {
                         if check_head_char(other, '-') {
                             println!("无效参数\"{}\"", other);
+                            thread::sleep(time::Duration::from_secs(1));
                             continue;
                         } else {
-                            let mut path_part = vec![other];
-                            path_part.extend(s_line.clone());
-                            path = path_part.join(" ");
-                            false
+                            let mut path = PathBuf::new();
+                            let mut path_str = String::new();
+                            loop {
+                                match s_line.next() {
+                                    Some(str) => {
+                                        if !path_str.is_empty() {
+                                            path_str.push_str(" ");
+                                        }
+                                        path_str = path_str + &str;
+                                        continue;
+                                    }
+                                    None => {
+                                        path.push(path_str);
+                                        break;
+                                    }
+                                }
+                            }
+                            if check_head_char(other, '/') {
+                                full_path.push(path);
+                            } else {
+                                full_path = PathBuf::from(
+                                    audio_dir().unwrap().to_string_lossy().to_string(),
+                                );
+                                full_path.push(path);
+                            }
                         }
+                        false
                     }
                     None => {
                         println!("缺少有效路径！");
+                        thread::sleep(time::Duration::from_secs(1));
                         continue;
                     }
                 };
-                let base_path = audio_dir().unwrap().to_string_lossy().to_string();
-                if is_loop {
-                    let mut path_part: Vec<&str> = vec![s_line.next().unwrap()];
-                    path_part.extend(s_line.clone());
-                    let full = path_part.join(" ");
-                    if let Some(path_next3) = Some(full) {
-                        if check_head_char(&path_next3, '/') {
-                            return BackPathMode(path_next3.to_string(), is_loop);
-                        } else {
-                            return BackPathMode(
-                                (base_path + "/" + &path_next3).to_string(),
-                                is_loop,
-                            );
-                        }
-                    } else {
-                        println!("缺少有效路径！");
-                        continue;
-                    }
-                } else {
-                    if check_head_char(&path, '/') {
-                        return BackPathMode(path.to_string(), is_loop);
-                    } else {
-                        return BackPathMode((base_path + "/" + &path).to_string(), is_loop);
-                    }
-                }
-            } else {
-                println!("未能匹配到指令\"{}\"", play);
+                return BackPathMode(full_path, is_loop);
+            }
+            Some(other) => {
+                println!("无效参数\"{}\"", other);
+                thread::sleep(time::Duration::from_secs(1));
                 continue;
             }
-        } else {
-            continue;
+            None => {
+                continue;
+            }
         }
     }
 }
 pub fn file_playing_cmd(sink: &Sink) {
     let mut get_line = String::new();
-    std::io::stdin().read_line(&mut get_line).expect("无效指令！");
+    std::io::stdin()
+        .read_line(&mut get_line)
+        .expect("无效指令！");
     let get_line = get_line.trim();
     let mut s_line = get_line.split_whitespace();
     match s_line.next() {
@@ -78,7 +113,7 @@ pub fn file_playing_cmd(sink: &Sink) {
             Some("volume") => {
                 if let Ok(volume) = s_line.next().unwrap().trim().parse::<f32>() {
                     if let None = s_line.next() {
-                        if volume>=0.0 && volume<=1.0 {
+                        if volume >= 0.0 && volume <= 1.0 {
                             sink.set_volume(volume);
                         } else {
                             println!("音量大小必须在 0.0 ~ 1.0 之间！");
@@ -92,7 +127,7 @@ pub fn file_playing_cmd(sink: &Sink) {
                     println!("音量大小必须是一个有效数字！");
                     thread::sleep(time::Duration::from_secs(1));
                 }
-            },
+            }
             Some(other) => {
                 println!("未能匹配到指令\"{}\"", other);
                 thread::sleep(time::Duration::from_secs(1));
@@ -103,7 +138,7 @@ pub fn file_playing_cmd(sink: &Sink) {
             }
         },
         Some(other) => {
-            println!("未能匹配到指令\"{}\"",other);
+            println!("未能匹配到指令\"{}\"", other);
             thread::sleep(time::Duration::from_secs(1));
         }
         None => {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use crate::file_mode::BackPathMode;
-use file_mode::{play_cmd,file_playing_cmd};
+use clearscreen::clear;
+use file_mode::BackPathMode;
+use file_mode::{file_playing_cmd, play_cmd};
+use panels::{start_menu, state_panel};
 use rodio::{Decoder, OutputStream, Sink, Source};
 use std::fs::File;
-use panels::{start_menu,mode_selected,state_panel};
-use clearscreen::clear;
 use std::thread;
 use std::time;
 mod file_mode;
@@ -13,12 +13,10 @@ fn main() {
     loop {
         clear().unwrap();
         let read_mode = start_menu();
-        clear().unwrap();
-        mode_selected();
         if read_mode == "file" {
             let BackPathMode(path, is_loop) = play_cmd();
             let (_stream, stream_controller) = OutputStream::try_default().unwrap();
-            let audio_file = File::open(path).unwrap();
+            let audio_file = File::open(&path).unwrap();
             let controller = Sink::try_new(&stream_controller).unwrap();
             let audio_src = Decoder::new(audio_file).unwrap();
             controller.set_volume(0.3);
@@ -28,7 +26,7 @@ fn main() {
                 println!("正在循环播放指定音乐...");
                 while !controller.empty() {
                     clear().unwrap();
-                    state_panel(&controller);
+                    state_panel(&controller, &path);
                     file_playing_cmd(&controller);
                 }
                 println!("播放结束");
@@ -39,7 +37,7 @@ fn main() {
                 println!("正在播放一次指定音乐...");
                 while !controller.empty() {
                     clear().unwrap();
-                    state_panel(&controller);
+                    state_panel(&controller, &path);
                     file_playing_cmd(&controller);
                 }
                 println!("播放结束");
